84eb8606e82b2b6f1f4439610b905f5b8eb10751
Jackson Merle <jackson.merle@gmail.com>
2024-11-12 10:16:03 +0900

    Initial Commit

---
+++ BIGBRAIN/BIGBRAINApp.swift
@@ -0,0 +1,17 @@
+//
+//  BIGBRAINApp.swift
+//  BIGBRAIN
+//
+//  Created by Jackson Merle on 2024/11/12.
+//
+
+import SwiftUI
+
+@main
+struct BIGBRAINApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
